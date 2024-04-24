71a7924a9a54b56c4890ff995338803e08c837b3
umitotuki <umitukiuk@gmail.com>
2024-04-24 11:12:55 +0000

    update5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
   end
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -34,4 +35,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+
 end
