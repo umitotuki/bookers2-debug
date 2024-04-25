3da06d038d59ac7c23dc5a3e893e7908dcf55014
umitotuki <umitukiuk@gmail.com>
2024-04-25 12:20:21 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
+
 
   def show
     @book = Book.find(params[:id])
@@ -46,4 +48,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update, :update]
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @user = User.find(params[:id])
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
 
   def user_params
@@ -34,7 +35,7 @@ class UsersController < ApplicationController
 
   def ensure_correct_user
     user = User.find(params[:id])
-    unless user.id == current_user
+    unless user == current_user
       redirect_to user_path(current_user)
     end
   end
