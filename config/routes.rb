8e099bb576417bbe58c47ece49243f8b0c9fd9c4
umitotuki <umitukiuk@gmail.com>
2024-04-26 23:38:08 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] 
+    resource :favorite, only: [:create, :destroy]
   resources :users, only: [:index,:show,:edit,:update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
