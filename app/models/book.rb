507e8d0cfccfcbb062734d273dea485507948a6a
umitotuki <umitukiuk@gmail.com>
2024-04-26 23:18:16 +0000

    favorite

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  has_many :favorites, dependent: :destroy
 end
