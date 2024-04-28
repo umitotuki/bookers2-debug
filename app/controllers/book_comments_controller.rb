0bae09f0750d4cb32c90614674d72d21b40fc62f
umitotuki <umitukiuk@gmail.com>
2024-04-28 23:49:16 +0000

    comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -16,7 +16,7 @@ class BookCommentsController < ApplicationController
 
   private
 
-  def post_comment_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 
