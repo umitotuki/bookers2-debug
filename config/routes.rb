a9d65741f166ab1aba0b67ce9896f6173da780cd
umitotuki <umitukiuk@gmail.com>
2024-05-05 01:16:32 +0000

    group

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'groups/new'
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
@@ -15,5 +19,7 @@ Rails.application.routes.draw do
   end
 
   get "/search", to: "searches#search"
+  
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
