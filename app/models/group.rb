663e1cecaf8588ccee0c01960a2afafd441d59d2
umitotuki <umitukiuk@gmail.com>
2024-05-08 06:11:45 +0000

    group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   belongs_to :owner, class_name: "User"
+  has_many :users, through: :group_users, source: :user
 
   validates :name, presence: true
   validates :introduction, presence: true
@@ -13,4 +14,10 @@ class Group < ApplicationRecord
   def is_owned_by?(user)
     owner.id == user.id
   end
+  
+  def includesUser?(user)
+    group_users.exists?(user_id: user.id)
+  end
+
+
 end
