206a0807ce514a1b370f4fecacab8677c9fbdfc7
umitotuki <umitukiuk@gmail.com>
2024-05-09 03:54:22 +0000

    SNS

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,12 +2,11 @@ class Group < ApplicationRecord
   has_one_attached :group_image
   has_many :group_users, dependent: :destroy
   belongs_to :owner, class_name: "User"
-  
+
   has_many :users, through: :group_users, source: :user
 
   validates :name, presence: true
   validates :introduction, presence: true
-  
 
   def get_group_image
     (group_image.attached?) ? group_image : 'no_image.jpg'
@@ -16,7 +15,7 @@ class Group < ApplicationRecord
   def is_owned_by?(user)
     owner.id == user.id
   end
-  
+
   def includesUser?(user)
     group_users.exists?(user_id: user.id)
   end
