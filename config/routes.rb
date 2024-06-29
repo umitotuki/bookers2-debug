bc3ccf60f21980e2877c195d0bd97c8e52d25b9c
umitotuki <umitukiuk@gmail.com>
2024-06-29 13:46:30 +0000

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,9 @@ Rails.application.routes.draw do
     resource :group_users, only: [:create, :destroy]
   end
   
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+  
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
