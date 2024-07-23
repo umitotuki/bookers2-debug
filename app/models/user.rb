2208f8119caf43a2ee88c71e774fff72213b6490
umitotuki <umitukiuk@gmail.com>
2024-07-23 02:49:41 +0000

    モデル記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ GUEST_USER_EMAIL = "guest@example.com"
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :group_users, dependent: :destroy
+  has_many :read_counts, dependent: :destroy
 
   def follow(user)
     active_relationships.create(followed_id: user.id)
