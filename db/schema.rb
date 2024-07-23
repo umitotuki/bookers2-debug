7b663617236e394c4f8ad631f1ee60a58be333b4
umitotuki <umitukiuk@gmail.com>
2024-07-23 02:45:02 +0000

    閲覧数モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_28_112042) do
+ActiveRecord::Schema.define(version: 2024_07_23_023922) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,13 @@ ActiveRecord::Schema.define(version: 2024_06_28_112042) do
     t.integer "user_id", null: false
     t.integer "room_id", null: false
     t.text "message", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "read_counts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
