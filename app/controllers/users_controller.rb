9ccde0127d4edbe3a7b60058a3b9671192640423
umitotuki <umitukiuk@gmail.com>
2024-07-30 11:10:28 +0000

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.page(params[:page]).reverse_order
     @book = Book.new
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
